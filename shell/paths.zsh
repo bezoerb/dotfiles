86b0eecb39c972b7d5bcfcbec453385a7fa38c54
Ben Zörb <ben@sommerlaune.com>
2016-02-20 21:21:07 +0100

    2nd round with fixes

--- shell/paths.zsh
+++ shell/paths.zsh
@@ -1,10 +1,12 @@
 # Directories to be prepended to PATH
-declare -a dirs_to_prepend=(
-    "/usr/local/bin" # Ensure that this bin always takes precedence over `/usr/bin`
+declare -a dirs_to_prepend
+dirs_to_prepend=(
+    "/usr/local/bin"
 )
 
 # Directories to be appended to PATH
-declare -a dirs_to_append=(
+declare -a dirs_to_append
+dirs_to_append=(
     "/usr/bin"
     "$HOME/bin"
     "$HOME/.dotfiles/bin"
@@ -13,11 +15,11 @@ declare -a dirs_to_append=(
     "$(brew --prefix coreutils)/libexec/gnubin" # Add brew-installed GNU core utilities bin
     "$(brew --prefix)/share/npm/bin" # Add npm-installed package bin
     "$(brew --prefix homebrew/php/php70)/bin" # Add php
-
 )
 
+
 # Prepend directories to PATH
-for dir in ${(k)dirs_to_prepend}
+for dir in ${(k)dirs_to_prepend[@]}
 do
     if [ -d ${dir} ]; then
         # If these directories exist, then prepend them to existing PATH
@@ -26,7 +28,7 @@ do
 done
 
 # Append directories to PATH
-for dir in ${(k)dirs_to_append}
+for dir in ${(k)dirs_to_append[@]}
 do
     if [ -d ${dir} ]; then
         # If these bins exist, then append them to existing PATH
