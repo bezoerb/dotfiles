e650b6242a1ac0b3b62673a4174bb7ed8bfd6960
Ben Zörb <benjamin.zoerb@jvm.de>
2024-06-27 23:52:36 +0200

    Adds fzf config

--- shell/exports.zsh
+++ shell/exports.zsh
@@ -43,3 +43,15 @@ eval "$(fnm env)"
 
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 export PUPPETEER_EXECUTABLE_PATH=`which chromium`
+
+export JAVA_HOME=`/usr/libexec/java_home -v 17`
+
+FD_OPTIONS="--follow --exclude .git --exclude node_modules"
+
+export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window='right:hidden:wrap' --bind='f3:execute(bat --style=numbers {}} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | pbcopy)'"
+
+# Use git-ls-files inside git repo, otherwise fd
+export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type l $FD_OPTIONS"
+export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
+export FZF_ALT_C_COMMAND="fd --type d $FDOPTIONS"
+export BAT_PAGER="less -R"
